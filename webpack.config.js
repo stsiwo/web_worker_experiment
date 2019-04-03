377dcf5558d5edfdb199ab67c961fefc1e2d2675
sts-msys <stsiwo@gmail.com>
2019-04-03 10:06:31 -0700

    configed webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack'); // to access built-in plugins
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js", 
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: __dirname,
+    hot: true,
+    historyApiFallback: true,
+    port: 8081
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'web workers',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+      template: 'src/index.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    // options for resolving module requests
+    // (does not apply to resolving to loaders)
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "app")
+    ],
+    // directories where to look for modules
+    extensions: [".js", ".json", ".jsx", ".css"],
+  }
+}
