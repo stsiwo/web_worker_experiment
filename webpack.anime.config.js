4815a6331ab11190fd6c7d1b2ef00c03b733d793
sts-msys <stsiwo@gmail.com>
2019-04-06 18:08:01 -0700

    configed webpack and html for three.js

---
+++ webpack.anime.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack'); // to access built-in plugins
+
+module.exports = {
+  mode: "development",
+   entry: {
+     anime: './src/anime/anime.ts'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: __dirname,
+    hot: true,
+    historyApiFallback: true,
+    port: 8081
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'web workers',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+      template: 'src/anime/anime.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    // options for resolving module requests
+    // (does not apply to resolving to loaders)
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "app")
+    ],
+    // directories where to look for modules
+    extensions: [".js", ".json", ".jsx", ".css", "ts", "tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      { test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ]
+  }
+
+}
+
